34959cf27fcff0ad717304632e9a1c5871f12cef
mk360 <hamoudi191@hotmail.fr>
2021-12-11 00:38:11 +0100

    implement working example with formidable

--- server/index.js
+++ server/index.js
@@ -2,14 +2,29 @@ const getIPAddress = require("./getIpAddress");
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const formidable = require("formidable");
 
 const server = http.createServer(async (req, res) => {
+    const form = formidable();
+    if (req.method === "POST") {
+        form.parse(req, (err, fields, files) => {
+            const ws = fs.createWriteStream("./test");
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(files.file.filepath);
+                fs.writeFileSync("./test.png", fs.readFileSync(files.file.filepath, 'binary'), 'binary');
+                res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+                res.end(JSON.stringify({ "bruh":  false }));
+            }
+        });
+        return;
+    }
+
     try {
         if (req.url.includes(".ico")) return;
-        if (req.method === "POST") {
-            await recoverData(req);
-            res.statusCode = 200;
-        }
         const filePath = path.join(__dirname, '../client' + (req.url === "/" || req.url.includes("fileupload") ? "/index.html" : req.url));
         const { size } = fs.statSync(filePath);
 
@@ -30,19 +45,6 @@ const server = http.createServer(async (req, res) => {
 
     }
 });
-
-function recoverData(req) {
-    return new Promise((resolve, reject) => {
-        let data = "";
-        req.on("data", (ch) => {
-            data += ch.toString();
-        });
-
-        req.on("end", () => {
-            const s = JSON.parse(data);
-        });
-    });
-};
 
 server.listen(3000, () => {
     console.log("listening at IP Address " + getIPAddress() + ", port 3000");
