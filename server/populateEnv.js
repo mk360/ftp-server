c81913a5e4ad2e6253e9b0fc84124767c3df5390
mk360 <hamoudi191@hotmail.fr>
2021-12-11 01:28:29 +0100

    handle .env file nonexistence

--- server/populateEnv.js
+++ server/populateEnv.js
@@ -1,16 +1,21 @@
 // basically reproducing what the dot-env package did
 const fs = require("fs");
 const os = require("os");
-const env = fs.readFileSync('../.env', 'utf-8');
-const entries = env.split(os.EOL).filter(e => e).map(i => i.trim());
-const entriesRegex = /^(?<entryName>[A-z-_]+)\s*=\s*(?<entryValue>.+)/;
 
-for (let entry of entries) {
-    const entriesRegexResult = entriesRegex.exec(entry);
-    if (entriesRegexResult) {
-        const { entryName, entryValue } = entriesRegexResult.groups;    
-        if (!process.env[entryName]) {
-            process.env[entryName] = entryValue;
+try {
+    const env = fs.readFileSync('../.env', 'utf-8');
+    const entries = env.split(os.EOL).filter(e => e).map(i => i.trim());
+    const entriesRegex = /^(?<entryName>[A-z-_]+)\s*=\s*(?<entryValue>.+)/;
+
+    for (let entry of entries) {
+        const entriesRegexResult = entriesRegex.exec(entry);
+        if (entriesRegexResult) {
+            const { entryName, entryValue } = entriesRegexResult.groups;    
+            if (!process.env[entryName]) {
+                process.env[entryName] = entryValue;
+            }
         }
     }
+} catch (e) {
+    console.log(".env file not found at project root. Configured settings will not be taken into account.");
 }
